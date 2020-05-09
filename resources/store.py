f1d6b3bc85b78bcd500614798509406e9297460c
lordphoenix <rajmishra@RAJs-MacBook-Pro.local>
2020-05-09 22:40:17 +0530

    Initial Commit

---
+++ resources/store.py
@@ -0,0 +1,33 @@
+from flask_restful import Resource,reqparse
+from flask_jwt import jwt_required
+import sqlite3
+from models.store import StoreModel
+
+class Store(Resource):
+
+    @jwt_required()
+    def get(self, name):
+        store = StoreModel.find_by_name(name)
+        if store:
+            return store.json()
+        return {'message': 'Store not found'}, 404
+    
+    def post(self,name):
+        if StoreModel.find_by_name(name):
+            return {"message" : "An store with name {} already exisits".format(name)}, 400
+        store = StoreModel(name)
+        try:
+            store.save_to_db()
+        except:
+            return {"message" : "An error occured while inserting the store"}, 500
+        return store.json(),201
+    
+    def delete(self, name):
+        item = ItemModel.find_by_name(name)
+        if item:    
+            item.delete_from_db()
+        return {'message' : 'Item has been deleted'}
+
+class StoreList(Resource):
+    def get(self):
+        return {'stores' : [store.json() for store in StoreModel.query.all()]}
