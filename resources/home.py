0a4185d735a6159371b23f485c0f9c57b3de5ec1
lordphoenix <rajmishra@RAJs-MacBook-Pro.local>
2020-05-10 14:33:38 +0530

    Bug Fix

--- resources/home.py
+++ resources/home.py
@@ -1,6 +1,6 @@
 from flask_restful import Resource,reqparse
 from flask_jwt import jwt_required
 
-class HomePage(Resources):
-    def get(self, name):
+class HomePage(Resource):
+    def get(self):
         return {'message': 'Please use Postman to test the endpoints'}, 200
